87f0027dc1807153b4c3fac6d6eaf4f02306184c
Kevin Avila <kevin.avila@avantgarde.de>
2022-01-20 13:20:41 +0100

    Adding tests for [POST] /login

---
+++ tests/recipes.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest');
+const bcrypt = require('bcrypt');
+const app = require('../app');
+const Users = require('../database/models/users');
+const mongoose = require('../database/dbConection');
+let recipeId;
+let userToken;
+
+const $http = request(app);
+const userCredentials = {
+  username: 'admin',
+  password: 'secret',
+};
+
+describe('Test the recipes API', () => {
+  beforeAll(async ()  => {
+    const password = bcrypt.hashSync('secret', 10);
+    await Users.create({
+      username: 'admin',
+      password,
+    });
+  });
+
+  afterAll(async () => {
+    await Users.deleteMany();
+    mongoose.disconnect();
+  });
+
+  describe('[POST] /login', () => {
+    it('Authenticazes User and sign them in', async () => {
+      const { body, statusCode } = await $http.post('/login').send(userCredentials);
+
+      userToken = body.accessToken;
+      expect(statusCode).toEqual(200);
+      expect(body).toEqual(
+        expect.objectContaining({
+          accessToken: body.accessToken,
+          success: true,
+          data: expect.objectContaining({
+            ...body.data
+          }),
+        }),
+      );
+    });
+  });
+});
